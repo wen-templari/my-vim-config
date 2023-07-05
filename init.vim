d2ac5639fe121239aa4143b195c08ab85b1c10a8
Wen Rex <ewr1szh@bosch.com>
2023-07-05 13:58:19 +0800

    save

--- init.vim
+++ init.vim
@@ -3,7 +3,7 @@ set ruler
 syntax on
 
 set cursorline
-highlight CursorLine term=bold cterm=bold guibg=White
+" highlight CursorLine term=bold cterm=bold guibg=White
 
 set encoding=UTF-8
 
@@ -15,7 +15,7 @@ set smarttab
 set autoindent
 set smartindent
 
-colorscheme kanagawa
+" colorscheme onedark
 set termguicolors
 set signcolumn=yes
 
@@ -27,28 +27,6 @@ nmap <s-tab> V<
 vmap <tab> >gv
 vmap <s-tab> <gv
 
-" pair brackets...
-imap ( ()<ESC>i
-imap () ()<ESC>a
-
-imap [ []<ESC>i
-imap [] []<ESC>a
-
-imap {<SPACE> {<SPACE><SPACE>};<ESC><LEFT><LEFT>i
-imap {; {<CR><CR>};<ESC>jddkkddkA
-imap { {}<ESC>i<CR><CR><ESC>kA<TAB><RIGHT><RIGHT>
-imap {} {}<ESC>a
-
-
-imap < <><ESC>i
-imap <<SPACE> <<ESC><RIGHT>r<SPACE>a
-imap << <<<ESC><RIGHT>r<SPACE>a<BACKSPACE>
-imap <= <=<ESC>a
-imap <> <><ESC>a
-
-imap "" ""<ESC>i
-imap '' ''<ESC>i
-
 lua << EOF
 require('main')
 EOF
@@ -56,6 +34,6 @@ EOF
 nnoremap <leader>ff <cmd>Telescope find_files<cr>
 nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-nnoremap <leader>gf <cmd>lua =vim.lsp.buf.formatting_sync()<cr>
+nnoremap <leader>gf <cmd>lua =vim.lsp.buf.format()<cr>
 nnoremap <leader>tt <cmd>NvimTreeToggle<cr>
 
--- lua/main.lua
+++ lua/main.lua
@@ -1,13 +1,92 @@
-require('plugins')
-require('plugin-config/nvim-tree')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { 'f-person/git-blame.nvim' },
+  { 'jiangmiao/auto-pairs'},
+  { 'tpope/vim-surround' },
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  },
+  { 'nvim-tree/nvim-web-devicons',      lazy = true },
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup({})
+    end
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    config = function()
+      require('nvim-tree').setup()
+    end
+  },
+  {
+    'akinsho/bufferline.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    config = function()
+      require('bufferline').setup()
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.0',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' }
+    }
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate'
+  },
+  { 'williamboman/mason.nvim' },
+  { 'williamboman/mason-lspconfig.nvim' },
+  { 'neovim/nvim-lspconfig' },
+  { 'onsails/lspkind.nvim' },
+  {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip'
+  },
+  { 'github/copilot.vim' },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priroity = 1000
+  },
+  {
+    'navarasu/onedark.nvim',
+    config = function()
+      require('onedark').setup({
+        stye = 'darker',
+        transparent = true
+      })
+      require('onedark').load()
+    end
+  }
+})
 
 require('plugin-config/lsp')
 require('plugin-config/nvim-cmp')
-
-require('bufferline').setup()
---
--- require('onedark').setup {
---   stye = 'darker',
---   transparent = true
--- }
--- require('onedark').load()
