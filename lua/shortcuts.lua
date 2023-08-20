165fba7da4eb454abaf24ed22e27b135a4915778
templari <q79142466@163.com>
2023-08-20 15:39:29 +0800

    Update shortcuts

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -10,4 +10,16 @@ map('n', '<leader>tt', ':NvimTreeToggle<CR>', opt)
 map('n', '<leader>tb', ':GitBlameToggle<CR>', opt)
 
 map('n', '<leader>gf', '<cmd>lua=vim.lsp.buf.format()<CR>', opt)
-map('n', '<leader>gh', '<cmd>lua=vim.lsp.buf.hover()<CR>', opt)
+
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opt)
+map('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opt)
+map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opt)
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opt)
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opt)
+
+map('n', 'go', '<cmd>lua vim.diagnostic.open_float()<CR>', opt)
+map('n', 'gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opt)
+map('n', 'gn', '<cmd>lua vim.diagnostic.goto_next()<CR>', opt)
+
+map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opt)
+map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opt)
